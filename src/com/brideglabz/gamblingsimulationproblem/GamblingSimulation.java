5e8eac7cb5a9b51af5956584ff0e8883f8da9393
usha-12 <usha12ahirwar@gmail.com>
2023-01-16 21:41:36 +0530

    Added:  Calculative Gambler if won or lost 50% of the stake, resigns for the day

--- src/com/brideglabz/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/brideglabz/gamblingsimulationproblem/GamblingSimulation.java
@@ -9,6 +9,7 @@ public class GamblingSimulation {
         GamblingSimulation gambler = new GamblingSimulation();
         gambler.gameInitialise();
         gambler.makeBet();
+        gambler.gamble();
     }
     public void gameInitialise() {
         System.out.println("\nInitialising game...");
@@ -38,7 +39,18 @@ public class GamblingSimulation {
             System.out.println("Bet LOST!");
         System.out.println("Funds : $"+funds);
     }
+
+    public void gamble() {
+        int minimumFund = funds - (funds/2);
+        int maximumFund = funds + (funds/2);
+        while(funds < maximumFund && funds > minimumFund) {
+            makeBet();
+        }
+        System.out.println("\nPlayer resigns for the day.");
+    }
 }
-/*As a Gambler make $1
-bet so either win or
-loose $1*/
+/*As a Calculative
+Gambler if won or lost
+50% of the stake,
+would resign for the
+day*/
