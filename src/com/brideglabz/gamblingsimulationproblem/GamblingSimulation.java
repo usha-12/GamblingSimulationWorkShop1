b32f6597c0493538d8bdceee4418c950ba6310df
usha-12 <usha12ahirwar@gmail.com>
2023-01-16 21:31:09 +0530

    Added: Gambler makes  bet and either wins or loses

--- src/com/brideglabz/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/brideglabz/gamblingsimulationproblem/GamblingSimulation.java
@@ -3,10 +3,12 @@ package com.brideglabz.gamblingsimulationproblem;
 public class GamblingSimulation {
     public int funds = 0;
     public int bet = 0;
+    public int wins = 0;
     public static void main(String[] args) {
         System.out.println("---------------Welcome Gambling Simulation Problem-------------------");
         GamblingSimulation gambler = new GamblingSimulation();
         gambler.gameInitialise();
+        gambler.makeBet();
     }
     public void gameInitialise() {
         System.out.println("\nInitialising game...");
@@ -14,8 +16,29 @@ public class GamblingSimulation {
         bet = 1;
         System.out.println("Funds : $"+funds);
     }
+    public void makeBet() {
+        double result = Math.random();
+        boolean betResult = false;
+        if (result > 0.5) {
+            funds += bet;
+            wins++;
+            betResult = true;
+        }
+        else {
+            funds -= bet;
+        }
+        displayResults(betResult);
+    }
+
+    public void displayResults(boolean result) {
+        System.out.println();
+        if(result)
+            System.out.println("Bet WON!");
+        else
+            System.out.println("Bet LOST!");
+        System.out.println("Funds : $"+funds);
+    }
 }
-/*As a Gambler, would
-start with a stake of
-$100 every day and bet
-$1 every game.*/
+/*As a Gambler make $1
+bet so either win or
+loose $1*/
